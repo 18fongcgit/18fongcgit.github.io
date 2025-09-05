d779d2e0381ccfc21efe1942c0e319075d4d0677
18fongcgit <afongfanhs@gmail.com>
2025-09-05 13:16:00 -0600

    Removed about and projects section to add on a later date, added more styling and organized JSX

--- 18fongcgitapp/src/App.tsx
+++ 18fongcgitapp/src/App.tsx
@@ -1,50 +1,62 @@
 import './App.css'
-import Item from './components/Item'
+// import Item from './components/Item'
 import githubIconWhite from './assets/github-mark-white.png'
+import githubIconBlack from './assets/github-mark.png'
+
+import { useState } from 'react'
+import MoonIcon from './components/Moonicon'
+import SunIcon from './components/Sunicon'
+
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    const currentTheme = isDarkMode ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+  };
 
   return (
     <div className='App'>
-      <div className='NavBar'>
-        
-        <a id="githubIcon" href='https://github.com/18fongcgit'><img src={githubIconWhite} width={60} height={60} alt='GitHub Logo' /></a>
-        <nav>
+      <nav className='NavBar'>
+        <div className='NavBarLeft'>
+          <a id="HomePage" href='/'><h2>Alex Fong</h2></a>
           <ul>
             <li><a href='#about'>About</a></li>
             <li><a href='#projects'>Projects</a></li>
           </ul>
-        </nav>
-      </div>
+        </div>
+        <a id="githubIcon" href='https://github.com/18fongcgit'><img src={isDarkMode ? githubIconBlack : githubIconWhite} width={60} height={60} alt='GitHub Logo' /></a>
+
+      </nav>
       <div className="Introduction">
-        <h1>Hello! I'm Alex Fong</h1>
-        <h2>Welcome to my website!</h2>
-        <div className="About Me">
-          <h3>About Me</h3>
-          <p>
-            I am a junior at the Colorado School of Mines, pursuing Computer Science with a
-            focus on Robotics and Intelligent Systems and a minor in Electrical Engineering. I am passionate
-            about robots, software, and electronics, and I am particularly fascinated by the applications of
-            Computer Vision and other sensors.
-
-            I am also an IB Diploma Recipient. As part of the IB Diploma program,
-            I developed a Solder Workshop class that allowed younger students with no experience in
-            Electronics to solder and create a LED lightboard with a customizable flash pattern.
-          </p>
-          <img src='https://via.placeholder.com/150' alt='LED lightboard example' />
-
-
+        <div className='IntroText'>
+          <h1>Hello! I'm Alex Fong</h1>
+          <h2>Welcome to my website!</h2>
         </div>
-        <div className="Projects">
-          <h3>Projects</h3>
-          <ul>
-            <a href=''><Item></Item></a>
-            <a href=''><Item></Item></a>
-            <a href=''><Item></Item></a>
-            <a href=''><Item></Item></a>
-          </ul>
-        </div>
+        <img src={githubIconWhite} alt='Placeholder image' />
       </div>
+      <footer className="FooterBar" >
+        <div className='FooterText'>
+          <p>© 2025 Alex Fong. All rights reserved.</p>
+        </div>
+        <button id="Theme" onClick={toggleTheme}>
+          {isDarkMode ? (
+        <>
+          <MoonIcon className="sunIcon"  />
+          Dark Mode
+        </>
+      ) : (
+        <>
+          <SunIcon className="moonIcon" />
+          Light Mode
+        </>
+      )}
+        </button>
+
+      </footer>
     </div>
 
   )
